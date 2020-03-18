dafa78d10b6735a461c3cacf3c7193747ecc6c9a
anhduc109 <ducpham1098@gmail.com>
2020-03-18 17:20:14 +0200

    test env var

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -3,13 +3,13 @@ import fs from 'fs'
 
 import logger from './logger'
 
-if (fs.existsSync('.env')) {
-  logger.debug('Using .env file to supply config environment variables')
-  dotenv.config({ path: '.env' })
-} else {
-  logger.debug('Using .env.example file to supply config environment variables')
-  dotenv.config({ path: '.env.example' }) // you can delete this after you create your own .env file!
-}
+// if (fs.existsSync('.env')) {
+//   logger.debug('Using .env file to supply config environment variables')
+//   dotenv.config({ path: '.env' })
+// } else {
+//   logger.debug('Using .env.example file to supply config environment variables')
+//   dotenv.config({ path: '.env.example' }) // you can delete this after you create your own .env file!
+// }
 export const ENVIRONMENT = process.env.NODE_ENV
 const prod = ENVIRONMENT === 'production' // Anything else is treated as 'dev'
 
