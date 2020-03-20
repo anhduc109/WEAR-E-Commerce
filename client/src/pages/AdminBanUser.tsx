8bae2a4f7e7e0c24ea3bae516bd17dacedd9984a
anhduc109 <ducpham1098@gmail.com>
2020-03-20 16:17:50 +0200

    add admin ban user feature

--- client/src/pages/AdminBanUser.tsx
+++ client/src/pages/AdminBanUser.tsx
@@ -1,17 +1,93 @@
 import React, { useState, useEffect } from 'react'
-import { Typography } from '@material-ui/core'
+import { useSelector } from 'react-redux'
+import {
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@material-ui/core'
 import axios from 'axios'
+
+import { AppState, User } from '../types'
+import AdminUserTableRow from '../components/AdminUserTableRow'
 
 const AdminBanUser = () => {
   const [users, setUsers] = useState([])
+  const userEmail = useSelector((state: AppState) => state.user.user?.email)
+  const token = useSelector((state: AppState) => state.user.token)
 
-  useEffect(() => {})
+  async function fetchUser() {
+    if (token !== '') {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+      const res = await axios.get(
+        'https://e-clothing-api.herokuapp.com/api/v1/users',
+        config
+      )
+      setUsers(res.data)
+    }
+  }
+
+  useEffect(() => {
+    fetchUser()
+  }, [token])
+
+  const handleBanOrUnbanOnClick = async (userId: string) => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+    await axios.put(
+      'https://e-clothing-api.herokuapp.com/api/v1/users',
+      { userId },
+      config
+    )
+
+    const res = await axios.get(
+      'https://e-clothing-api.herokuapp.com/api/v1/users',
+      config
+    )
+    setUsers(res.data)
+  }
 
   return (
     <div className="page-wrapper">
       <Typography variant="h3" align="center">
         Users List
       </Typography>
+      <br />
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Username</TableCell>
+            <TableCell>Products in Cart</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>First Name</TableCell>
+            <TableCell>Last Name</TableCell>
+            <TableCell>Ban Status</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users
+            .filter((user: any) => user.email !== userEmail)
+            .map((user: any) => {
+              return (
+                <AdminUserTableRow
+                  key={user.email}
+                  user={user}
+                  handleBanOrUnbanOnClick={handleBanOrUnbanOnClick}
+                />
+              )
+            })}
+        </TableBody>
+      </Table>
     </div>
   )
 }
